cfe3872136da62c9dc5f4c43b1ea683be6f227b0
lusai <68563799@qq.com>
2020-01-10 10:29:10 +0800

    初始化

---
+++ src/main/java/cn/itcast/service/UserService.java
@@ -0,0 +1,11 @@
+package cn.itcast.service;
+
+import cn.itcast.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+}
---
+++ src/test/java/cn/itcast/ItcastServiceProviderApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcastServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
